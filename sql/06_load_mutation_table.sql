142bbe5fc6c9a9644318784e1c0e203b3f1203fa
Keertana Chagari <167469141+keertanac@users.noreply.github.com>
2026-05-08 15:39:07 -0400

    Update 06_load_mutation_table.sql

--- sql/06_load_mutation_table.sql
+++ sql/06_load_mutation_table.sql
@@ -1,2 +1,2 @@
 #file too large
-#download from here: https://drive.google.com/file/d/1Oh9ebdSYCqv3TBRAfWSGA4gODtCtWfWa/view?usp=drive_link
+#download from here: https://drive.google.com/file/d/1SSuNsCkDaWAwTy_cwc_cqZp5B65uBlVh/view?usp=drive_link
